91a14a95ac10f8ceff0ccdce719a6f1c3316f6e1
bladeonfire2402 <liemp966@gmail.com>
2025-04-11 23:59:07 +0700

    Change title

--- src/screens/home-page/index.tsx
+++ src/screens/home-page/index.tsx
@@ -30,9 +30,9 @@ const HomepageScreen = ({}: HomepageProps) => {
       <div className=" background-gradient h-screen flex  flex-col justify-center items-center text-center ">
         {HandleRenderSplash()}
         <div className="home-container">
-          <h1 className="text-5xl font-bold text-pink-600 mb-4">Lotus Reverie</h1>
+          <h1 className="text-5xl font-bold text-pink-600 mb-4">Lumora</h1>
           <p className="text-lg text-gray-700 mb-6">
-            Welcome to Lotus Reverie photobooth! This is your personal photobooth at home.
+            Welcome to Lumora photobooth! This is your personal photobooth at home.
           </p>
           <div style={{ height: "20px" }}></div>
 
@@ -45,11 +45,7 @@ const HomepageScreen = ({}: HomepageProps) => {
           <button onClick={() => router.push("/welcome")} className="transition">
             START
           </button>
-
-          
-          <div className="footer"></div>
-          
-          
+          <div className="footer"></div>          
         </div>
       </div>
     </HomepageWrapper>
