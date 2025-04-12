2a70b0418cf54c757aa9ff5be50246a573becaf9
bladeonfire2402 <liemp966@gmail.com>
2025-04-12 12:08:48 +0700

    Sửa splash screen

--- src/screens/splash/index.tsx
+++ src/screens/splash/index.tsx
@@ -24,15 +24,19 @@ const SplashScreen = ({ visible }: SplashScreenProps) => {
     <div className={`splash-screen-wrapper ${visible ? "visible" : ""} absolute z-[2000] bg-white w-full top-0 left-0 h-screen text-wheat`}>
       <div className="splash-content flex flex-col justify-center items-center h-full">
         <div className={`flex gap-2 splashTitle h-[300px] ${black_hans.className} `}>
-          <h1>L</h1>
-          <h1>u</h1>
-          <h1>m</h1>
+          <h1>P</h1>
           <h1>o</h1>
-          <h1>r</h1>
-          <h1>a</h1>
+          <h1>s</h1>
+          <h1>e</h1>
+          <h1>-</h1>
+          <h1>n</h1>
+          <h1>-</h1>
+          <h1>S</h1>
+          <h1>e</h1>
+          <h1>n</h1>
         </div>
         <div className={`subtitle ${poppins.className} `}>
-          Where Moments Blossom
+         Where every pose meets a petal.
         </div>
 
       </div>
